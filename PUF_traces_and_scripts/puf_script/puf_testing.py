3fecad5c7eea7c8c817f6fa0bf078acd5c12cdaa
Gustav Grahn <ggrahn@kth.se>
2025-03-17 19:19:56 +0100

    added testing

--- PUF_traces_and_scripts/puf_script/puf_testing.py
+++ PUF_traces_and_scripts/puf_script/puf_testing.py
@@ -28,7 +28,7 @@ input_size = 150
  
 data_folder = "../traces/puf0_avg100/"
 model_folder = "../models/puf0/"
-modelName = 'puf0'
+modelName = 'puf0.h5'
 
 #data_folder = "../traces/puf12_avg100/"
 #model_folder = "../models/puf12/"
@@ -40,90 +40,118 @@ modelName = 'puf0'
 ################################################### 
 
 def load_sca_model(model_folder,model_file):
-	for f_name in os.listdir(model_folder):
-		if f_name.startswith(model_file):
-			model = load_model(model_folder+f_name)
-			print(f_name)
-			#print(model.summary())
-		else:
-			continue
-	return model
+    for f_name in os.listdir(model_folder):
+        if f_name.startswith(model_file):
+            model = load_model(model_folder+f_name)
+            print(f_name)
+            #print(model.summary())
+        else:
+            continue
+    return model
 
 def rank_func(model, dataset, labels):
 
-	# Get the input layer shape
-	input_layer_shape = model.get_layer(index=0).input_shape
+    # Get the input layer shape
+    input_layer_shape = model.get_layer(index=0).input_shape
 
-	if len(input_layer_shape) == 2:
-		# This is a MLP
-		input_data = dataset
-	elif len(input_layer_shape) == 3:
-		# This is a CNN: reshape the data
-		input_data = dataset
-		input_data = input_data.reshape((input_data.shape[0], input_data.shape[1], 1))
-	else:
-		print("Error: model input shape length %d is not expected ..." % len(input_layer_shape))
-		sys.exit(-1)
+    if len(input_layer_shape) == 2:
+        # This is a MLP
+        input_data = dataset
+    elif len(input_layer_shape) == 3:
+        # This is a CNN: reshape the data
+        input_data = dataset
+        input_data = input_data.reshape((input_data.shape[0], input_data.shape[1], 1))
+    else:
+        print("Error: model input shape length %d is not expected ..." % len(input_layer_shape))
+        sys.exit(-1)
 
-	# Predict probabilities for 2-node output case
-	predictions = model.predict(input_data)
-	predictions = np.argmax(predictions,axis=1)
+    # Predict probabilities for 2-node output case
+    predictions = model.predict(input_data)
+    predictions = np.argmax(predictions,axis=1)
 
-	# xor = 0 if the same
-	ranks_prd = np.bitwise_xor(predictions, labels)
+    # xor = 0 if the same
+    ranks_prd = np.bitwise_xor(predictions, labels)
 
-	return (ranks_prd)
+    return (ranks_prd)
+
 
 # Load testing data
-def load_traces(): 
+def load_traces(repetitions):
+    # Load traces and labels for the specified number of repetitions
+    traces = np.load(data_folder+'trace.npy')[:repetitions,:]  # [0:1000,:]
+    labels = np.load(data_folder+'label.npy')[:repetitions]  # [0:1000]
+    print('traces shape:', traces.shape)
+    print('labels shape:', labels.shape)
 
-	traces = np.load(data_folder+'trace9.npy')#[0:1000,:] 
-	labels = np.load(data_folder+'label9.npy')#[0:1000]
-	
-	print('traces shape:', traces.shape)
-	print('labels shape:', labels.shape)
+    important_trace_points = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 85, 86, 87, 88, 89, 90, 91, 92, 127, 128, 129, 130, 131, 132]
+    traces = traces[:, important_trace_points]  # Select only these 25 points
 
-	important_trace_points = [0, 1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 85, 86, 87, 88, 89, 90, 91, 92, 127, 128, 129, 130, 131]
-	traces = traces[:, important_trace_points]
+    print('traces shape after selection:', traces.shape)
 
-	# Scale (standardize) traces
-	delimitedTraces = np.zeros(traces.shape)
-	for x_index in range(traces.shape[0]):
-		delimitedTraces[x_index,:] = -1+(traces[x_index,:]-np.min(traces[x_index,:]))*2/(np.max(traces[x_index,:])-np.min(traces[x_index,:]))
+    # Scale (standardize) traces
+    delimitedTraces = np.zeros(traces.shape)
+    for x_index in range(traces.shape[0]):
+        delimitedTraces[x_index,:] = -1 + (traces[x_index,:] - np.min(traces[x_index,:])) * 2 / (np.max(traces[x_index,:]) - np.min(traces[x_index,:]))
 
-	return (delimitedTraces, labels)
+    return (delimitedTraces, labels)
 
 # Check a saved model against test traces
-def check_model(model_folder,model,count):
-	# Load attack data
-	(traces, labels) = load_traces()
+def check_model(model_folder, model, count):
+    # Load model
+    model_load = load_sca_model(model_folder, model)
+    all_predictions = []
+    all_true_labels = []
 
-	# Load model
-	model_load = load_sca_model(model_folder,model)
+    # Load traces and labels for the current repetition
+    traces, labels = load_traces(1000)
 
+    # Create a new trace with shape (0, 150) for the average trace
+    #new_trace = np.zeros((0, 25))
 
-	# coorect if 0
-	num_attack = int(labels.shape[0])
-	ranks_prd = np.zeros((num_attack, 1))
-	ranks_prd = rank_func(model_load, traces, labels)
+    # Compute the average trace
+    #average_trace = np.mean(traces, axis=0)
+    #new_trace = np.vstack((average_trace, new_trace))
 
-	# Calcultate probability of correct prediction
-	a = 1-np.mean(ranks_prd)
-	print('test accuracy:', a)
-	print('mean of lables:', np.mean(labels))
+    new_trace = traces
 
-	return
-	
+    # Correct if 0
+    num_attack = int(labels.shape[0])
+    ranks_prd = np.zeros((num_attack, 1))
+
+    # Change the trace depending on the strategy
+    ranks_prd = rank_func(model_load, new_trace, labels)
+
+    # Calculate classification accuracy for the current repetition
+    classification_accuracy = 1 - np.mean(ranks_prd)
+
+    # Print the classification accuracy for the average trace
+    print(classification_accuracy)
+
+    # Append classification accuracy and true labels
+    # Used to get the sum of log
+    all_predictions.append(classification_accuracy)
+    all_true_labels.append(np.mean(labels))
+
+    # Combine predictions using sum of logs
+    predictions = np.sum(np.log(all_predictions), axis=0)
+
+    # Calculate probability of correct prediction
+    a = np.exp(predictions)
+    print('test accuracy:', a)
+    print('mean of labels:', all_true_labels)
+
+    return
+    
 start = time.time()
 print('input size =', input_size)
-        					
+                            
 # Test all models in model_folder
 count = 0
 for file_name in os.listdir(model_folder):
-	if fnmatch.fnmatch(file_name, '*.h5'):  # Check that file is a model
-		model = file_name
-		count = count + 1
-		check_model(model_folder,model,count)
+    if fnmatch.fnmatch(file_name, '*.h5'):  # Check that file is a model
+        model = file_name
+        count = count + 1
+        check_model(model_folder,model,count)
 
 end = time.time()
 print("The total running time was: ",((end-start)/60), " minutes.") 
--- PUF_traces_and_scripts/puf_script/puf_training.py
+++ PUF_traces_and_scripts/puf_script/puf_training.py
@@ -4,7 +4,7 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import Model, Sequential
-from keras.layers import Dense, InputLayer, BatchNormalization, ReLU, Softmax
+from keras.layers import Dense, InputLayer, BatchNormalization, ReLU, Softmax, Dropout
 from keras.engine.topology import get_source_inputs
 from keras.utils import layer_utils
 from keras.utils.data_utils import get_file
@@ -19,14 +19,6 @@ from keras.models import load_model
 import time
 
 ################################################
-#data_folder = "../traces/puf0_avg100/"
-#model_folder = "../models/puf0/"
-#modelName = 'puf0'
-
-#data_folder = "../traces/puf12_avg100/"
-#model_folder = "../models/puf12/"
-#modelName = 'puf12'
-
 data_folder = "../traces/puf0_avg0_blocks_of_1000/"
 model_folder = "../models/puf0/"
 modelName = 'puf0_avg0_blocks_of_1000'
@@ -35,123 +27,122 @@ input_size = 25
 #################################################
 
 def create_model(classes=2, input_size=25):
-	input_shape = (25,)
+    input_shape = (25,)
 
-	# Create model.
-	model = Sequential()
-	model.add(BatchNormalization(input_shape=input_shape))
+    # Create model.
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=input_shape))
 
-	model.add(Dense(4, kernel_initializer='he_uniform', input_shape=input_shape))
-	model.add(BatchNormalization())
-	model.add(ReLU())
+    model.add(Dense(4, kernel_initializer='he_uniform', input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(ReLU())
+    model.add(Dropout(0.5))  # Add dropout for regularization
 
-	#model.add(Dense(2, kernel_initializer='he_uniform', input_shape=input_shape))
-	#model.add(BatchNormalization())
-	#model.add(ReLU())
+    model.add(Dense(classes, kernel_initializer='he_uniform', input_dim=32))
+    model.add(Softmax())
 
-	model.add(Dense(classes, kernel_initializer='he_uniform', input_dim=32))
-	model.add(Softmax())
+    optimizer = Nadam(lr=0.001, epsilon=1e-08)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-	optimizer = Nadam(lr=0.001, epsilon=1e-08)
-	model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-
-	# print(model.summary())
-	return model
+    return model
 
 def check_file_exists(file_path):
-	if os.path.exists(file_path) == False:
-		print("Error: provided file path '%s' does not exist!" % file_path)
-		sys.exit(-1)
-	return
+    if os.path.exists(file_path) == False:
+        print("Error: provided file path '%s' does not exist!" % file_path)
+        sys.exit(-1)
+    return
 
 def load_traces():
+    # Import our traces
+    traces = np.load(data_folder + 'trace.npy')
+    labels = np.load(data_folder + 'label.npy')
+    for i in range(1, 1):
+        tempTrace = np.load(data_folder + 'trace' + str(i) + '.npy')
+        tempLabel = np.load(data_folder + 'label' + str(i) + '.npy')
+        traces = np.append(traces, tempTrace, axis=0)
+        labels = np.append(labels, tempLabel, axis=0)
 
-	# Import our traces
-	# Choose here which files to use
-	traces = np.load(data_folder+'trace.npy')
-	labels = np.load(data_folder+'label.npy')
-	for i in range(1,1):  
-		tempTrace = np.load(data_folder+'trace'+str(i)+'.npy')
-		tempLabel = np.load(data_folder+'label'+str(i)+'.npy')
-		traces = np.append(traces,tempTrace,axis=0)
-		labels = np.append(labels,tempLabel,axis=0)
-	
-	print('traces shape:', traces.shape)
-	print('labels shape:', labels.shape)
+    print('traces shape:', traces.shape)
+    print('labels shape:', labels.shape)
 
-	# Scale (standardize) traces
-	# Select the best leakage points based on t-test results
-	important_trace_points = [0, 1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 85, 86, 87, 88, 89, 90, 91, 92, 127, 128, 129, 130, 131]
-	traces = traces[:, important_trace_points]	
+    # Scale (standardize) traces
+    important_trace_points = [0, 1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 85, 86, 87, 88, 89, 90, 91, 92, 127, 128, 129, 130, 131]
+    traces = traces[:, important_trace_points]
 
-	delimitedTraces = np.zeros(traces.shape)
-	for x_index in range(traces.shape[0]):
-		delimitedTraces[x_index,:] = -1+(traces[x_index,:]-np.min(traces[x_index,:]))*2/(np.max(traces[x_index,:])-np.min(traces[x_index,:]))
+    delimitedTraces = np.zeros(traces.shape)
+    for x_index in range(traces.shape[0]):
+        delimitedTraces[x_index, :] = -1 + (traces[x_index, :] - np.min(traces[x_index, :])) * 2 / (np.max(traces[x_index, :]) - np.min(traces[x_index, :]))
 
-	# **Sequential split (last 10% as test set)**
-	num_samples = len(delimitedTraces)
-	split_index = int(0.9 * num_samples)
+    # Shuffle the data before splitting
+    indices = np.arange(len(delimitedTraces))
+    np.random.shuffle(indices)
+    delimitedTraces = delimitedTraces[indices]
+    labels = labels[indices]
 
-	X_train, X_test = delimitedTraces[:split_index], delimitedTraces[split_index:]
-	Y_train, Y_test = labels[:split_index], labels[split_index:]
+    # Sequential split (last 10% as test set)
+    num_samples = len(delimitedTraces)
+    split_index = int(0.9 * num_samples)
 
-	print(f"Dataset split: {split_index} training samples, {num_samples - split_index} testing samples")
+    X_train, X_test = delimitedTraces[:split_index], delimitedTraces[split_index:]
+    Y_train, Y_test = labels[:split_index], labels[split_index:]
 
-	return X_train, X_test, Y_train, Y_test
-	###############################################################
+    print(f"Dataset split: {split_index} training samples, {num_samples - split_index} testing samples")
+
+    return X_train, X_test, Y_train, Y_test
 
 def train_model(X_profiling, Y_profiling, model, save_file_name, epochs=50, batch_size=64):
-	check_file_exists(os.path.dirname(save_file_name))
-	# Save model every epoch
-	save_model = ModelCheckpoint(save_file_name+'.h5',monitor='val_acc',verbose=1,save_best_only=True,mode='max')
-	# Get the input layer shape
-	input_layer_shape = model.get_layer(index=0).input_shape
-	# Sanity check
-	if input_layer_shape[1] != len(X_profiling[0]):
-		print("Error: model input shape %d instead of %d is not expected ..." % (input_layer_shape[1], len(X_profiling[0])))
-		sys.exit(-1)
-	# Adapt the data shape according our model input
-	if len(input_layer_shape) == 2:
-		# This is a MLP
-		Reshaped_X_profiling = X_profiling
-	elif len(input_layer_shape) == 3:
-		# This is a CNN: expand the dimensions
-		Reshaped_X_profiling = X_profiling.reshape((X_profiling.shape[0], X_profiling.shape[1], 1))
-	else:
-		print("Error: model input shape length %d is not expected ..." % len(input_layer_shape))
-		sys.exit(-1)
+    check_file_exists(os.path.dirname(save_file_name))
+    save_model = ModelCheckpoint(save_file_name + '.h5', monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    input_layer_shape = model.get_layer(index=0).input_shape
 
-	es = EarlyStopping(monitor='val_acc', mode='max', verbose=1, patience=20)
+    if input_layer_shape[1] != len(X_profiling[0]):
+        print("Error: model input shape %d instead of %d is not expected ..." % (input_layer_shape[1], len(X_profiling[0])))
+        sys.exit(-1)
 
-	history = model.fit(x=Reshaped_X_profiling, y=to_categorical(Y_profiling, num_classes=2), batch_size=batch_size, verbose=0, epochs=epochs, callbacks=[es,save_model], validation_split=0.3)
-	
-	# Uncomment this to see how accuracy changes per epoch
-	# # summarize history for accuracy
-	plt.plot(history.history['acc'])
-	plt.plot(history.history['val_acc'])
-	plt.title('model accuracy')
-	plt.ylabel('accuracy')
-	plt.xlabel('epoch')
-	plt.legend(['train', 'test'], loc='upper left')
-	plt.savefig('../history/puf0/' + modelName + '.pdf')
-	plt.show()
-	
-	return history
-	  
-# Start of execution, the time parts are there for our own references so we know roughly how long training takes
+    if len(input_layer_shape) == 2:
+        Reshaped_X_profiling = X_profiling
+    elif len(input_layer_shape) == 3:
+        Reshaped_X_profiling = X_profiling.reshape((X_profiling.shape[0], X_profiling.shape[1], 1))
+    else:
+        print("Error: model input shape length %d is not expected ..." % len(input_layer_shape))
+        sys.exit(-1)
+
+    es = EarlyStopping(monitor='val_acc', mode='max', verbose=1, patience=20)
+
+    history = model.fit(x=Reshaped_X_profiling, y=to_categorical(Y_profiling, num_classes=2), batch_size=batch_size, verbose=0, epochs=epochs, callbacks=[es, save_model], validation_split=0.3)
+
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig('../history/puf0/' + modelName + '.pdf')
+    plt.show()
+
+    return history
+
+# Start of execution
 start = time.time()
 
 # Load the training traces
 X_train, X_test, Y_train, Y_test = load_traces()
 
-### MLP training
-
+# MLP training
 mlp = create_model(input_size=input_size)
-
-train_model(X_train, Y_train, mlp, model_folder+modelName, epochs=100, batch_size=128)
+train_model(X_train, Y_train, mlp, model_folder + modelName, epochs=100, batch_size=128)
 
 end = time.time()
-print("The total running time was: ",((end-start)/60), " minutes.") 
+print("The total running time was: ", ((end - start) / 60), " minutes.")
+
+# Evaluate on the full test set
 test_loss, test_acc = mlp.evaluate(X_test, to_categorical(Y_test, num_classes=2))
 print("Test Accuracy:", test_acc)
+
+# Evaluate on a small subset (e.g., 10 samples)
+random_indices = np.random.choice(len(X_test), 10, replace=False)
+X_sample = X_test[random_indices]
+Y_sample = Y_test[random_indices]
+sample_loss, sample_acc = mlp.evaluate(X_sample, to_categorical(Y_sample, num_classes=2))
+print("Sample Accuracy:", sample_acc)
 
